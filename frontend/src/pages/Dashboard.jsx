ee7177e1618fe829f01ae93d9e506d9ebd01e7ec
jay7040 <jayp08036@gmail.com>
2025-02-24 17:46:04 -0500

    Creator and Dashboard pages

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import MyProfile from "../dashboard/MyProfile";
+import MyBlogs from "../dashboard/MyBlogs";
+import CreateBlog from "../dashboard/CreateBlog";
+import UpdateBlog from "../dashboard/UpdateBlog";
+import { Navigate } from "react-router-dom";
+function Dashboard() {
+  const { profile, isAuthenticated } = useAuth();
+  const [component, setComponent] = useState("My Blogs");
+  console.log(profile);
+  console.log(isAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <div>
+      <div>
+        <Sidebar component={component} setComponent={setComponent} />
+        {component === "My Profile" ? (
+          <MyProfile />
+        ) : component === "Create Blog" ? (
+          <CreateBlog />
+        ) : component === "Update Blog" ? (
+          <UpdateBlog />
+        ) : (
+          <MyBlogs />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
